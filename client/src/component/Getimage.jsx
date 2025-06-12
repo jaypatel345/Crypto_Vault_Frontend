b5c7c2c4ac4027f7f25e1c1d217b4c920f5f9187
Jay patel <77409923+jaypatel345@users.noreply.github.com>
2025-06-12 11:39:34 +0530

    new update

--- client/src/component/Getimage.jsx
+++ client/src/component/Getimage.jsx
@@ -1,4 +1,5 @@
 const Getimage = () => {
+    return(<>Get image</>);
 }
  
 export default Getimage;
--- client/src/component/Uploadimage.jsx
+++ client/src/component/Uploadimage.jsx
@@ -1,18 +1,33 @@
 import axios from "axios";
+import { useState } from "react";
 
 const Uploadimage = () => {
-    const handleuploadimage = async () => {
-        try {
-          const url = `http://localhost:3000/api/uploadimage`;
-          const res = await axios.get(url);
-          console.log(res.data);
-        } catch (err) {
-          console.error("Axios upload failed:", err.response?.data || err.message);
-        }
-      };
+    const [file, setFile] = useState(null);
 
+  const handleuploadimage = async () => {
+    try {
+        const formData = new FormData();
+        formData.append("file",file)
+      const url = `http://localhost:3000/api/uploadimage`;
+      const res = await axios.post(url, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      console.log(res.data);
+    } catch (err) {
+      console.error("Axios upload failed:", err.response?.data || err.message);
+    }
+  };
+  console.log(file);
   return (
     <div>
+      <input
+        type="file"
+        accept=".jpg, .jpeg, .png"
+        onChange={(e) => setFile(e.target.files[0])}
+        className="w-[200px] md:w-[210px]"
+      />
       <button onClick={handleuploadimage}>uploadimage</button>
     </div>
   );
