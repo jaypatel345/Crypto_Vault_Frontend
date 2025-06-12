10ade1800a7845aec6672f1d62f65d0cc3685685
Jay patel <77409923+jaypatel345@users.noreply.github.com>
2025-06-12 10:00:01 +0530

    update new file

--- client/src/component/Uploadimage.jsx
+++ client/src/component/Uploadimage.jsx
@@ -1,11 +1,15 @@
 import axios from "axios";
 
 const Uploadimage = () => {
-  const handleuploadimage = async () => {
-    const url = `http://localhost:3000/api/uploadimage`;
-    const res = await axios.get(url);
-    console.log(res.data);
-  };
+    const handleuploadimage = async () => {
+        try {
+          const url = `http://localhost:3000/api/uploadimage`;
+          const res = await axios.get(url);
+          console.log(res.data);
+        } catch (err) {
+          console.error("Axios upload failed:", err.response?.data || err.message);
+        }
+      };
 
   return (
     <div>
